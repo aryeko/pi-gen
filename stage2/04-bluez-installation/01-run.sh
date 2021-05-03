48ffda11c9f696e98401fbcef232f306c5fb7b3a
Arye Kogan <aryekogan@gmail.com>
2021-05-03 11:44:07 +0300

    fixing {}

--- stage2/04-bluez-installation/01-run.sh
+++ stage2/04-bluez-installation/01-run.sh
@@ -3,14 +3,14 @@ BLUEZ_VER=5.54
 echo "Installing bluez dependencies"
 sudo apt-get install libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev -y
 
-echo "Downloading bluez version {$BLUEZ_VER}"
-wget www.kernel.org/pub/linux/bluetooth/bluez-{$BLUEZ_VER}.tar.xz
-tar xvf bluez-{$BLUEZ_VER}.tar.xz && cd bluez-{$BLUEZ_VER}
+echo "Downloading bluez version $BLUEZ_VER"
+wget www.kernel.org/pub/linux/bluetooth/bluez-$BLUEZ_VER.tar.xz
+tar xvf bluez-$BLUEZ_VER.tar.xz && cd bluez-$BLUEZ_VER
 
-echo "Building bluez version {$BLUEZ_VER}"
+echo "Building bluez version $BLUEZ_VER"
 ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --enable-experimental
 make -j4
 
-echo "Installing bluez version {$BLUEZ_VER}"
+echo "Installing bluez version $BLUEZ_VER"
 sudo make install
 
